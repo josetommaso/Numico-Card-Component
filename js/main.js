3713f0c973d8b9412a2474836b934c1ffb86216b
Jose Tommaso <tommaso_j@hotmail.com>
2022-11-18 11:47:57 +0000

    countUp

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
+const counters = document.querySelectorAll('.counter');
+
 AOS.init({
 	delay: 1000,
 	duration: 600,
+});
+
+var countOptions = {
+	useEasing: true,
+	separator: '',
+	duration: 2000,
+};
+
+counters.forEach((counter) => {
+	let count = new CountUp(counter.id, 0, counter.innerHTML, 0, 5, countOptions);
+
+	// start the counting after 1500ms
+	setTimeout(() => {
+		count.start();
+	}, 1500);
 });
